b512b43d3790b61fb9b4db67c6b2b4a747194c65
Will Y Tanaka <wt40173@srcs.loc>
2017-02-13 13:41:13 -0800

    task 2 and 4

--- functions_start-1.js
+++ functions_start-1.js
@@ -60,7 +60,7 @@ function multTable(row, col) {
  * @returns {number}
  */
 function factorial(n) {
-    var result = 0;
+    var result = 1;
     for (var i = n; i > 1; i--) {
         result = result * i;
     }
@@ -74,8 +74,8 @@ function factorial(n) {
  * @returns {number} nCk
  */
 function combinations(n, k) {
-    var c;
-    c = factorial(n) / factorial(k) * (factorial(n - k));
+
+  var  c = factorial(n) / (factorial(k) * factorial(n - k));
     return c;
 }
 
